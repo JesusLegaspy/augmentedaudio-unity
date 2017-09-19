19608f1e227171f874e84846fc87f630c9041988
JesusLegaspy <jesus.legaspy@utexas.edu>
2017-09-18 21:08:17 -0500

    Added Unity linux build

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -21,7 +21,7 @@ echo "Attempting to build $project for Windows"
 	-projectPath "$(pwd)" \
 	-buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
 	-quit
-
+'
 echo "Attempting to build $project for Linux"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
 	-batchmode \
@@ -31,13 +31,12 @@ echo "Attempting to build $project for Linux"
 	-projectPath "$(pwd)" \
 	-buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" \
 	-quit
-'
 
 echo "Logs from build"
 cat $(pwd)/unity.log
 
 
 echo "Attempting to zip builds"
-#zip -r $(pwd)/Build/linux.zip $(pwd)/Build/linux/
+zip -r $(pwd)/Build/linux.zip $(pwd)/Build/linux/
 #zip -r $(pwd)/Build/mac.zip $(pwd)/Build/osx/
 zip -r $(pwd)/Build/windows.zip $(pwd)/Build/windows/
