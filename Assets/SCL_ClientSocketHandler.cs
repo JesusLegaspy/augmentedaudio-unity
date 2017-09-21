546d2829331ed71133d41077b066d8c72f33bf47
Khalid Qarryzada <khalid.qarryzada@gmail.com>
2017-09-20 22:19:56 -0500

    Project now compiles but is structured improperly.

--- Assets/SCL_ClientSocketHandler.cs
+++ Assets/SCL_ClientSocketHandler.cs
@@ -6,28 +6,20 @@ using System.IO;
 using System.Text;
 using System.Threading;
 
-public interface SCL_IClientSocketHandlerDelegate
-{
+public interface SCL_IClientSocketHandlerDelegate {
 	// callback, will be NOT be invoked on main thread, so make sure to synchronize in Unity
 	void msgParse(SCL_ClientSocketHandler handler, string message);
 }
 
 public class SCL_ClientSocketHandler {
-
 	protected readonly TcpClient client;
 	protected readonly string separatorSequence;
 	protected readonly char [] separatorSequenceChars;
 	protected readonly Encoding encoding;
 	protected SCL_IClientSocketHandlerDelegate socketDelegate;
 	
-	public static Queue createFlags = new Queue();
-	public static Queue destroyFlags = new Queue();
-	
-	public SCL_ClientSocketHandler (TcpClient client, 
-	                                SCL_IClientSocketHandlerDelegate socketDelegate, 
-	                                string separatorString,
-	                                Encoding encoding)
-	{
+	public SCL_ClientSocketHandler (TcpClient client,  SCL_IClientSocketHandlerDelegate socketDelegate, 
+										string separatorString, Encoding encoding) {
 		this.client = client;
 		this.encoding = encoding;
 		this.separatorSequence = separatorString;
@@ -35,16 +27,14 @@ public class SCL_ClientSocketHandler {
 		this.socketDelegate = socketDelegate;
 	}	
 
-	public void Cleanup()
-	{
+	public void Cleanup() {
 		if (this.client != null) {
 			Debug.Log ("Socket client closed (cleanup) ");
 			this.client.Close();
 		}
 	}
-
-	public void Run()
-	{
+	
+	public void Run() {
 		try {
 			StringBuilder dataBuffer = new StringBuilder();
 
@@ -52,41 +42,22 @@ public class SCL_ClientSocketHandler {
 				byte [] read_buffer = new byte[256];
 				int read_length = this.client.Client.Receive(read_buffer);
 				if(read_length > 0) {
-					//createFlags.Enqueue(12);
-					//int yes = Convert.ToInt32(createFlags.Dequeue());
-					//Debug.Log("The value in the queue was " + yes);
 					dataBuffer.Append(this.encoding.GetString(read_buffer, 0, read_length));
 					string data = dataBuffer.ToString();
 					string [] tokens = data.Split(this.separatorSequenceChars);
 					for(int i = 0; i < tokens.Length; i++) {
 						this.socketDelegate.msgParse(this, tokens[i]);
 					}
-					dataBuffer = new StringBuilder();	//clear dataBuffer
-					int yes = Convert.ToInt32(createFlags.Dequeue());
-					Debug.Log("Read from queue successful. The value was: " + yes);
+					dataBuffer = new StringBuilder();	//clear dataBuffer before next read
 				}
-				
-				/*while (true == destroyFlags.Contains()) {
-					createFlags.dequeue();
-					UnityEngine.Object.Destroy(SCL_PositionalControllerInput.controlledObject);
-				}*/
-				//while (true == createFlags.Contains()) {
-				//}
 			}
-		} 
-		catch (ThreadAbortException exception) 
-		{
-			Debug.Log ("Thread aborted");
-		} 
-		catch (SocketException exception) 
-		{
-			Debug.Log ("Socket exception");
-		} 
-		finally 
-		{
+		} catch (ThreadAbortException e) {
+			Debug.LogError ("Thread aborted in client run loop: " + e);
+		}  catch (SocketException e) {
+			Debug.LogError ("Socket exception: " + e);
+		} finally {
 			this.client.Close();
 			Debug.Log ("Socket client closed " + this.client.Client.RemoteEndPoint);
 		}
 	}
-
 }
--- Assets/SCL_PositionalControllerInput.cs
+++ Assets/SCL_PositionalControllerInput.cs
@@ -1,23 +1,57 @@
 ﻿using UnityEngine;
 using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Globalization;
 using System.Text;
 
 public class SCL_PositionalControllerInput : MonoBehaviour, SCL_IClientSocketHandlerDelegate {
-
-	//public GameObject controlledObject;
-	//public GameObject cube;
-	public GameObject[] cubeArray;
+	public struct myObj {
+		public bool valid;
+		public GameObject cubeObj;		//GameObject object that is physically placed in the Unity Engine
+		public float[] position;		//3-element array containing position
+	};
+	public readonly static int maxObjects = 10;
+	private static myObj[] cubeArray = { new myObj(), new myObj(), new myObj(), new myObj(), new myObj(),
+											new myObj(), new myObj(), new myObj(), new myObj(), new myObj()};
+	
 	private readonly object positionLock = new object();
 	private Vector3 position = new Vector3();
 
 	private SCL_SocketServer socketServer;
-
+	
+	public int getUnusedID() {
+		int i = 0; 
+		while(i < maxObjects) {
+			if(false == cubeArray[i].valid)
+				return i;
+			i++;
+		}
+		return -1;		//all 10 objects in use
+	}
+	
+	private myObj get_myObj(int ID) {
+		return cubeArray[ID];
+	}
+	
+	//used to update or add object; does not check for overwrite.
+	private void update_myObj (myObj input, int ID) {
+		cubeArray[ID] = input;
+	}
+	
+	private void destroy_myObj(int ID) {
+		myObj dest = cubeArray[ID];
+		dest.valid = false;
+		Destroy(dest.cubeObj);
+	}
 	// Use this for initialization
 	void Start () {
-		for(int i = 0; i < 10; i++)
-			cubeArray[i] = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		for(int i = 0; i < maxObjects; i++) {
+			//myObj init = new myObj();
+			myObj init = cubeArray[i];
+			init.valid = false;
+			//this.update_myObj(init, i);
+		}
 		SCL_IClientSocketHandlerDelegate socketDelegate = this;
 		int maxClients = 1;
 		string separatorString = "+";
@@ -41,13 +75,15 @@ public class SCL_PositionalControllerInput : MonoBehaviour, SCL_IClientSocketHan
 	}
 
 	void FixedUpdate() {
-		this.controlledObject.transform.position = this.GetPositionValue();
+		for(int i = 0; i < maxObjects; i++)
+			if(true == cubeArray[i].valid)
+				cubeArray[i].cubeObj.transform.position = this.GetPositionValue();			
 	}
 
 	// this delegate method will be called on another thread, so use locks for synchronization
 	public void msgParse(SCL_ClientSocketHandler handler, string message) {
 		string[] pieces = message.Split('+');
-		if(pieces.Length > 10) {
+		if(pieces.Length > maxObjects) {
 			Debug.Log("Too many arguments to msgParse, terminating.");
 			return;
 		}
@@ -57,88 +93,72 @@ public class SCL_PositionalControllerInput : MonoBehaviour, SCL_IClientSocketHan
 		}
 	}
 	
+	//input is the entire instruction (e.g., M1(0.2,1.2,-1.3)
+	//output is string array of length 3 corresponding to x, y, and z
+	private float[] coordsToFloats(string input){
+		int ind1 = input.IndexOf("(");			//if not found?
+		int ind2 = input.IndexOf(")");
+		string coords = input.Substring(ind1 + 1, ind2 - ind1 - 1);
+		string[] pieces = coords.Split(',');
+		float[] retval = {0, 0, 0};
+		if (pieces.Length == 3) {
+			try {
+				retval[0] = float.Parse(pieces[0], CultureInfo.InvariantCulture);
+				retval[1] = float.Parse(pieces[1], CultureInfo.InvariantCulture);
+				retval[2] = float.Parse(pieces[2], CultureInfo.InvariantCulture);
+			} catch (FormatException e) {
+				Debug.LogError ("Invalid number format in input: " + e);
+			}
+		} else 
+			Debug.LogError ("Too many coordinates entered in command: " + input);
+		return retval;
+	}
+	
 	//args of the form An(x,y,z)
 	//	"A" can take a value of C (create), D (destroy), M (move)
 	//	"n" is the ID number of the object
 	private void execCmd(string input) {
 		string cmd = input.Substring(0, 1);
-		
-		//Debug.Log("coords contains: " + coords);
-		
-		//Debug.Log("The ID is: " + ID);
-		
-		//string coords = input.Substring(len + 2, 11);	//grabs coordinates, i.e., "1.0,1.0,1.0"
-		
-		//Debug.Log("The coordinates are: " + coords);
-		
-		string[] pieces = coords.Split(',');
-		float x = 0;
-		float y = 0;
-		float z = 0;
-		if (pieces.Length == 3) {
-			try {
-				x = float.Parse(pieces[0], CultureInfo.InvariantCulture);
-				y = float.Parse(pieces[1], CultureInfo.InvariantCulture);
-				z = float.Parse(pieces[2], CultureInfo.InvariantCulture);
-				//Debug.Log (String.Format("x: {0}, y: {1}, z: {2}", x, y, z));
-			} catch (FormatException e) {
-				Debug.Log ("Invalid number format: " + input);
-			}
-		} else 
-			Debug.Log ("Length is invalid: " + input);
-		
-		Debug.Log("x,y,z: " + x +  "," + y + "," + z);
-		
-		//Now that all the parsing of the message is done, execute the command based on what it is
 		switch (cmd) {
 			case "C": {
-				//Instantiate(this.cube, new Vector3(x, y, z), Quaternion.identity);
-				int newid = getUnusedID();
-				SCL_ClientSocketHandler.createFlags.Enqueue(newid);
+				GameObject newGO = GameObject.CreatePrimitive(PrimitiveType.Cube);;
+				float[] pos = coordsToFloats(input);
+				Instantiate(newGO, new Vector3(pos[0], pos[1], pos[2]), Quaternion.identity);
+				
+				myObj q = new myObj();
+				q.valid = true;
+				q.cubeObj = newGO;
+				q.position = pos;
+				update_myObj(q, getUnusedID());
 				break;
 			} case "D": {
+				int ID = -1;
 				try {
-					int id = int.Parse(input.Substring(1, 1));
+					ID = int.Parse(input.Substring(1, input.Length));
 				} catch (FormatException e) {
-					Debug.Log("Unable to Parse ID for Destroy: " + input.Substring(1,1));
+					Debug.LogError("ID could not be interpreted as integer: " + e);
 					return;
 				}
-				SCL_ClientSocketHandler.destroyFlags.Enqueue(ID);
+				destroy_myObj(ID);
 				break;
 			} case "M": {
-				//Debug.Log("The command is: " + cmd);
-				int ind1 = input.IndexOf("(");
-				int ind2 = input.IndexOf(")");
-				string coords = input.Substring(ind1 + 1, ind2 - ind1 - 1);
-				
-				//Find length of ID. This is found by taking the overall length, subtracting the 
-				//  coordinates and parentheses, and subtracting the cmd character.
-				int len = input.Length - (ind2 - ind1 + 1) - 1;
-				string ID_string = input.Substring(1, len);
+				float[] pos = coordsToFloats(input);
+				int len1 = input.IndexOf("(");
 				int ID;
 				try {
-					ID = int.Parse(ID_string);
+					ID = int.Parse(input.Substring(1, len1));
 				} catch (FormatException e) {
-					Debug.Log("String subset could not be transfererd to integer: " + ID_string + ", exiting.");
+					Debug.LogError("ID could not be interpreted as integer: " + e);
 					return;
 				}
-				this.SetPositionValue(x,y,z);
+				myObj upd = get_myObj(ID);
+				upd.position = pos;
 				break;
 			} default: {
 				Debug.Log("Cmd" + cmd + " not found.");
 				break;
 			}
 		}
-	}
-	
-	private int getUnusedID() {
-		int i = 0; 
-		while(i < 10) {
-			if(cubeArray[i] != -1)
-				return i;
-			i++;
-		}
-		return -1;		//all 10 objects in use
 	}
 	
 	protected Vector3 GetPositionValue() {
